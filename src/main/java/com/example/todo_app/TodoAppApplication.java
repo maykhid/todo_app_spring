6ed6cdc2cc23b649015015d7b88dde31c3b7a463
maykhid <henryifebunandu@gmail.com>
2024-07-11 23:24:11 +0100

    fix test

--- src/main/java/com/example/todo_app/TodoAppApplication.java
+++ src/main/java/com/example/todo_app/TodoAppApplication.java
@@ -1,7 +1,10 @@
 package com.example.todo_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TodoAppApplication {
@@ -10,4 +13,10 @@ public class TodoAppApplication {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
+	@Bean
+    CommandLineRunner printActiveProfiles(Environment env) {
+        return args -> {
+            System.out.println("Active profiles: " + String.join(", ", env.getActiveProfiles()));
+        };
+    }
 }
