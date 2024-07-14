1d4aefa2b450205d551758b4c0f2b2b73a6c79d1
maykhid <henryifebunandu@gmail.com>
2024-07-14 16:27:12 +0100

    update: init dummy data on success

--- src/main/java/com/example/todo_app/todo/TodoConfig.java
+++ src/main/java/com/example/todo_app/todo/TodoConfig.java
@@ -10,23 +10,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TodoConfig {
     
-    // @Bean
-    // CommandLineRunner commandLineRunner(TodoRepository todoRepository) {
-    //     return args -> {
-    //         Todo defTodo1 = new Todo(
-    //             "Buy milk",
-    //             "Buy milk at the store by 5pm",
-    //             LocalDate.now()
-    //         );
+    @Bean
+    CommandLineRunner commandLineRunner(TodoRepository todoRepository) {
+        return args -> {
+            Todo defTodo1 = new Todo(
+                "Buy milk",
+                "Buy milk at the store by 5pm",
+                LocalDate.now()
+            );
 
-    //         Todo defTodo2 = new Todo(
-    //             "Call Mum",
-    //             "Call Mum about her funds",
-    //             LocalDate.now()
-    //         );
+            Todo defTodo2 = new Todo(
+                "Call Mum",
+                "Call Mum about her funds",
+                LocalDate.now()
+            );
 
-    //         todoRepository.saveAll(List.of(defTodo1, defTodo2));
-    //     };
-    // }
+            todoRepository.saveAll(List.of(defTodo1, defTodo2));
+        };
+    }
 
 }
